a9bc1f5f6c418aeb5eb0858bfe805da9838d5333
nareshreddy123 <nareshreddyboreddy904@gmail.com>
2021-09-15 07:27:16 +1000

    hi

--- terrafom/instance/instance.tf
+++ terrafom/instance/instance.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "allow_tls" {
       cidr_blocks      = ["0.0.0.0/0"]
 
     }
-    }
+
 
   egress =
     {
@@ -42,7 +42,7 @@ resource "aws_security_group" "allow_tls" {
   tags = {
     Name = "allow_tls"
 }
-}
+
 
 
 
