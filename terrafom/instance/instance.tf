684bdfc1a8cf14eb042d71a5c5d70b6ee0b13de8
nareshreddy123 <nareshreddyboreddy904@gmail.com>
2021-09-15 07:18:36 +1000

    hi

--- terrafom/instance/instance.tf
+++ terrafom/instance/instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "myinstance" {
-  ami             = "ami-074df373d6bafa625"
-  instance_type   = "t2.micro"
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "HelloWorld"
@@ -12,4 +13,35 @@ provider "aws" {
 
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+}
 
