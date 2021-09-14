e3b2704f7db9e3672d9763d73d596dc235d65bde
nareshreddy123 <nareshreddyboreddy904@gmail.com>
2021-09-15 07:19:31 +1000

    hi

--- terrafom/instance/instance.tf
+++ terrafom/instance/instance.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "allow_tls" {
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
+
     }
   ]
 
