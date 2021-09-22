068417584912bbd7af8fbfffa4d0b48ebe47bcb5
nareshreddy123 <nareshreddyboreddy904@gmail.com>
2021-09-22 16:24:39 +1000

    hi

--- terrafom/terraform_vpc/vpc.tf
+++ terrafom/terraform_vpc/vpc.tf
@@ -1,11 +1,10 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.VPC_CIDR
+  cidr_block                       = var.VPC_CIDR
 }
-tags                        = {
-  Name                      = "vpc-${var.ENV}"
-  ENV                       = var.ENV
+      tags                         = {
+         Name                      = "vpc-${var.ENV}"
+         ENV                       = var.ENV
 }
 
 
 
-      
