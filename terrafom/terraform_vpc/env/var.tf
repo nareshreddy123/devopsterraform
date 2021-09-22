03c5d6f48da4c1b38d9c109a8e299d9c3b642478
nareshreddy123 <nareshreddyboreddy904@gmail.com>
2021-09-22 16:20:59 +1000

    hi

--- terrafom/terraform_vpc/env/var.tf
+++
@@ -1,1 +0,0 @@
-variable "VPC_CIDR" {}
---
+++ terrafom/terraform_vpc/vpc.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.VPC_CIDR
+}
+tags                        = {
+  Name                      = "vpc-${var.ENV}"
+  ENV                       = var.ENV
+}
+
+
+
+      
