9c371f96b6928e2dfdfbfb025096d269f3ea1acf
nareshreddy123 <nareshreddyboreddy904@gmail.com>
2021-09-22 17:34:02 +1000

    hi

---
+++ terrafom/remote/state.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "terrraform-devops"
+    key    = "sample/terrafrom.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform"
+  }
+}
+
+output "sample" {
+  value = "Hello World"
+}
+
+resource "null_resource" "sleep" {
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
+}
